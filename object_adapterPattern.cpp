1e309d0ea04c2ff6d19ab3844b6d613e7ed5f5bb
liu wei <liuwei888@mails.ccnu.edu.cn>
2015-09-01 11:20:18 +0800

    Create object_adapterPattern.cpp

---
+++ object_adapterPattern.cpp
@@ -0,0 +1,53 @@
+/*************************************************************************
+    > File Name: adapter2.cpp
+    > Author: liuwei
+    > Mail: 271551048@qq.com 
+    > Created Time: 2015年08月19日 星期三 16时05分16秒
+	  > 对象适配器模式
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Target
+{
+	public:
+		virtual void Request()
+		{
+
+		}
+};
+
+class Adaptee
+{
+	public:
+		void SpecificRequest()
+		{
+			cout << "Called SpecificRequest()" << endl;
+		}
+};
+
+//对象适配器模式只继承Target类
+class Adapter : public Target
+{
+	private:
+		Adaptee *adaptee;
+	public:
+		Adapter()
+		{
+			adaptee = new Adaptee;
+		}
+
+		void Request()
+		{
+			adaptee->SpecificRequest();
+		}
+};
+
+int main()
+{
+	Target *t = new Adapter;
+	t->Request();
+
+	return 0;
+}
